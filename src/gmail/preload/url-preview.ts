c28d6f6c11deef4af6d6eb77a418cec9a69d430a
Tim Cheung <tim@cheung.io>
2025-03-31 16:55:55 +0100

    fix url preview

--- src/gmail/preload/url-preview.ts
+++ src/gmail/preload/url-preview.ts
@@ -1,27 +1,66 @@
 import { GMAIL_URL } from "@/lib/constants";
 
 let urlPreviewElement: HTMLDivElement | null = null;
+let timeout: Timer | null = null;
+
+function lookupHref(target: HTMLElement) {
+	if (target instanceof HTMLAnchorElement) {
+		return target.href;
+	}
+
+	if (target.parentElement) {
+		return lookupHref(target.parentElement);
+	}
+
+	return null;
+}
 
 export function initUrlPreview() {
 	window.addEventListener("mouseover", (event) => {
-		const target = event.target as HTMLAnchorElement | HTMLElement;
-
-		if ("href" in target && !target.href.startsWith(GMAIL_URL)) {
-			urlPreviewElement = document.createElement("div");
-
-			urlPreviewElement.className = "meru-url-preview";
-
-			urlPreviewElement.style.color = "white";
-
-			urlPreviewElement.style.background = "black";
-
-			urlPreviewElement.textContent = target.href;
-
-			document.body.append(urlPreviewElement);
+		if (!(event.target instanceof HTMLElement)) {
+			console.warn("Target is not an HTMLElement");
+			return;
 		}
+
+		const href = lookupHref(event.target);
+
+		if (!href || href.startsWith(GMAIL_URL)) {
+			console.warn("URL is not a GMAIL URL");
+			return;
+		}
+
+		urlPreviewElement = document.createElement("div");
+
+		urlPreviewElement.style.position = "fixed";
+		urlPreviewElement.style.left = "0";
+		urlPreviewElement.style.bottom = "0";
+		urlPreviewElement.style.right = "0";
+		urlPreviewElement.style.maxWidth = "50%";
+		urlPreviewElement.style.padding = "6px";
+		urlPreviewElement.style.fontSize = "0.75rem";
+		urlPreviewElement.style.textOverflow = "ellipsis";
+		urlPreviewElement.style.overflow = "hidden";
+		urlPreviewElement.style.whiteSpace = "nowrap";
+		urlPreviewElement.style.color = "white";
+		urlPreviewElement.style.background = "black";
+
+		urlPreviewElement.textContent = href;
+
+		document.body.append(urlPreviewElement);
+
+		timeout = setTimeout(() => {
+			if (urlPreviewElement) {
+				urlPreviewElement.style.maxWidth = "";
+			}
+		}, 2000);
 	});
 
 	window.addEventListener("mouseout", () => {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = null;
+		}
+
 		if (urlPreviewElement) {
 			urlPreviewElement.remove();
 
