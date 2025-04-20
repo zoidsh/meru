f7361e7f547730a48069161e40f4a7aeb4390d65
Tim Cheung <tim@cheung.io>
2025-04-20 21:21:22 +0100

    fix url preview not disappearing sometimes

--- src/gmail/preload/url-preview.ts
+++ src/gmail/preload/url-preview.ts
@@ -1,5 +1,65 @@
 import { GMAIL_URL } from "@/lib/constants";
 
+let urlPreviewElement: HTMLElement | null = null;
+
+let removeUrlPreviewElementTimeout: Timer | null = null;
+
+function removeUrlPreviewElement() {
+	if (urlPreviewElement) {
+		urlPreviewElement.remove();
+		urlPreviewElement = null;
+	}
+}
+
+function fadeOutUrlPreviewElement() {
+	if (removeUrlPreviewElementTimeout) {
+		clearTimeout(removeUrlPreviewElementTimeout);
+
+		removeUrlPreviewElementTimeout = null;
+	}
+
+	if (urlPreviewElement) {
+		urlPreviewElement.addEventListener("animationend", removeUrlPreviewElement);
+
+		urlPreviewElement.setAttribute("data-fade-out", "true");
+	}
+}
+
+function createUrlPreviewElement(href: string) {
+	if (urlPreviewElement) {
+		urlPreviewElement.textContent = href;
+
+		if (urlPreviewElement.hasAttribute("data-fade-out")) {
+			urlPreviewElement.removeEventListener(
+				"animationend",
+				removeUrlPreviewElement,
+			);
+
+			urlPreviewElement.removeAttribute("data-fade-out");
+		}
+	} else {
+		urlPreviewElement = document.createElement("div");
+
+		urlPreviewElement.className = "url-preview";
+
+		urlPreviewElement.textContent = href;
+
+		document.body.append(urlPreviewElement);
+	}
+
+	if (removeUrlPreviewElementTimeout) {
+		clearTimeout(removeUrlPreviewElementTimeout);
+
+		removeUrlPreviewElementTimeout = null;
+	}
+
+	removeUrlPreviewElementTimeout = setTimeout(() => {
+		if (urlPreviewElement) {
+			urlPreviewElement.setAttribute("data-long-hover", "true");
+		}
+	}, 1500);
+}
+
 function lookupHref(target: HTMLElement) {
 	if (target instanceof HTMLAnchorElement) {
 		return target.href;
@@ -15,41 +75,34 @@ function lookupHref(target: HTMLElement) {
 export function initUrlPreview() {
 	window.addEventListener("mouseover", (event) => {
 		if (!(event.target instanceof HTMLElement)) {
+			fadeOutUrlPreviewElement();
+
 			return;
 		}
 
 		const href = lookupHref(event.target);
 
 		if (!href || href.startsWith(GMAIL_URL)) {
+			fadeOutUrlPreviewElement();
+
 			return;
 		}
 
-		const urlPreviewElement = document.createElement("div");
+		if (urlPreviewElement) {
+			urlPreviewElement.textContent = href;
 
-		urlPreviewElement.className = "url-preview";
+			if (urlPreviewElement.hasAttribute("data-fade-out")) {
+				urlPreviewElement.removeEventListener(
+					"animationend",
+					removeUrlPreviewElement,
+				);
 
-		urlPreviewElement.textContent = href;
-
-		document.body.append(urlPreviewElement);
-
-		const timeout = setTimeout(() => {
-			urlPreviewElement.setAttribute("data-long-hover", "true");
-		}, 1500);
-
-		const removeUrlPreviewElement = () => {
-			clearTimeout(timeout);
-
-			urlPreviewElement.onanimationend = () => {
-				urlPreviewElement.remove();
-			};
-
-			urlPreviewElement.setAttribute("data-fade-out", "true");
-
-			if (event.target) {
-				event.target.removeEventListener("mouseleave", removeUrlPreviewElement);
+				urlPreviewElement.removeAttribute("data-fade-out");
 			}
-		};
 
-		event.target.addEventListener("mouseleave", removeUrlPreviewElement);
+			return;
+		}
+
+		createUrlPreviewElement(href);
 	});
 }
