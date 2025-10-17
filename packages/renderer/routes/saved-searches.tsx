ebd95d73783d42127210e41a3949be7584e14106
Tim Cheung <tim@cheung.io>
2025-10-17 15:10:57 +0100

    fix saved search edit dialog title

--- packages/renderer/routes/saved-searches.tsx
+++ packages/renderer/routes/saved-searches.tsx
@@ -229,7 +229,7 @@ function SavedSearchMenuButton({
 			</DropdownMenu>
 			<DialogContent>
 				<DialogHeader>
-					<DialogTitle>Edit Account</DialogTitle>
+					<DialogTitle>Edit Saved Search</DialogTitle>
 				</DialogHeader>
 				<SavedSearchForm
 					savedSearch={savedSearch}
