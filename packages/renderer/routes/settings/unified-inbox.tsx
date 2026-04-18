cfaaa58f73e0b088dc138f1e47119844614af12c
Tim Cheung <tim@cheung.io>
2026-04-19 00:20:50 +0100

    hide other unified inbox options if disabled

--- packages/renderer/routes/settings/unified-inbox.tsx
+++ packages/renderer/routes/settings/unified-inbox.tsx
@@ -2,8 +2,15 @@ import { FieldGroup } from "@meru/ui/components/field";
 import { ConfigSwitchField } from "@/components/config-switch-field";
 import { LicenseKeyRequiredBanner } from "@/components/license-key-required-banner";
 import { Settings, SettingsContent, SettingsHeader, SettingsTitle } from "@/components/settings";
+import { useConfig } from "@meru/renderer-lib/react-query";
 
 export function UnifiedInboxSettings() {
+  const { config } = useConfig();
+
+  if (!config) {
+    return;
+  }
+
   return (
     <Settings>
       <SettingsHeader>
@@ -19,12 +26,14 @@ export function UnifiedInboxSettings() {
             licenseKeyRequired
             restartRequired
           />
-          <ConfigSwitchField
-            label="Show Sender Icons"
-            description="Show sender icons next to the senders in the unified inbox."
-            configKey="unifiedInbox.showSenderIcons"
-            licenseKeyRequired
-          />
+          {config["unifiedInbox.enabled"] && (
+            <ConfigSwitchField
+              label="Show Sender Icons"
+              description="Show sender icons next to the senders in the unified inbox."
+              configKey="unifiedInbox.showSenderIcons"
+              licenseKeyRequired
+            />
+          )}
         </FieldGroup>
       </SettingsContent>
     </Settings>
