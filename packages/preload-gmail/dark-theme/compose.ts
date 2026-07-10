9aa4faf9861842163af329c331c17d1cc55dbab0
Tim Cheung <tim@cheung.io>
2026-07-10 17:54:29 +0100

    theme fixes

--- packages/preload-gmail/dark-theme/compose.ts
+++ packages/preload-gmail/dark-theme/compose.ts
@@ -1,6 +1,7 @@
 import { applyDarkTheme, type DarkThemeController } from "@meru/dark-theme";
 import { GMAIL_PRELOAD_ARGUMENTS } from "@meru/shared/gmail";
 import { $$ } from "select-dom";
+import composeCss from "./compose.css";
 
 const isFullDarkThemeEnabled = process.argv.includes(GMAIL_PRELOAD_ARGUMENTS.fullDarkTheme);
 
@@ -30,6 +31,7 @@ export function darkThemeCompose() {
       composeElement,
       applyDarkTheme(composeElement, {
         backgroundColor: "rgb(19, 19, 19)",
+        css: composeCss,
       }),
     );
   }
