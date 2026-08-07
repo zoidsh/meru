063a8c9cdc1b251252c6e62286e27d6552fd411c
Tim Cheung <tim@cheung.io>
2026-08-07 18:28:15 +0200

    show service worker notifications from workspace apps

---
+++ packages/preload-workspace-app/service-worker-notifications.ts
@@ -0,0 +1,30 @@
+import { webFrame } from "electron";
+
+function patchShowNotification() {
+  ServiceWorkerRegistration.prototype.showNotification = function (title, options) {
+    try {
+      const notification = new Notification(title, {
+        body: options?.body,
+        icon: options?.icon,
+        tag: options?.tag,
+        silent: options?.silent,
+        data: options?.data,
+        requireInteraction: options?.requireInteraction,
+        lang: options?.lang,
+        dir: options?.dir,
+      });
+
+      notification.onclick = () => {
+        window.focus();
+      };
+
+      return Promise.resolve();
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  };
+}
+
+export function initServiceWorkerNotifications() {
+  webFrame.executeJavaScript(`(${patchShowNotification.toString()})()`);
+}
